948786fc378952fdf3088da9d44289c0ec553a77
asoderlind <axesode@gmail.com>
2023-07-09 08:29:00 +0900

    improve navigation and other basic things

--- tssite/src/App.tsx
+++ tssite/src/App.tsx
@@ -1,15 +1,21 @@
 import React, { ReactElement } from "react";
 import "./App.css";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
   Link,
+  Outlet,
 } from "react-router-dom";
 import { User } from "./models";
 
 import SignIn from "./components/LoginView";
+
+interface NavProps {
+  user: User | null;
+}
 
 function App() {
   return (
@@ -19,10 +25,19 @@ function App() {
   );
 }
 
+const ProtectedRoute = ({ user, redirectPath = "/login", children }: any) => {
+  if (!user) {
+    return <Navigate to={redirectPath} replace />;
+  }
+  return children ? children : <Outlet />;
+};
+
 function AppEntry() {
-  const [user, setUser] = React.useState<User>(
-    JSON.parse(sessionStorage.getItem("user") || "{}")
-  );
+  const [user, setUser] = React.useState<User | null>(null);
+  console.log(JSON.parse(sessionStorage.getItem("user") || "{}"));
+
+  const handleLogin = () => setUser({ id: 1, username: "robin" });
+  const handleLogout = () => setUser(null);
 
   function updateUserInfo(userDetail: User) {
     setUser(userDetail);
@@ -32,36 +47,56 @@ function AppEntry() {
 
   // A wrapper for <Route> that redirects to the login
   // screen if you're not yet authenticated.
-  const PrivateRoute = ({ component: Component, path, ...props }: any) => {
-    return (
-      <Route
-        path={path}
-        {...props}
-        element={
-          sessionStorage.getItem("loggedin") === "true" ? (
-            <Component />
-          ) : (
-            <Navigate to="/login" replace />
-          )
-        }
-      />
-    );
-  };
 
   return (
-    <section>
+    <>
+      <Navigation user={user} />
       <Routes>
-        <Route path="/login" element={<div>login</div>}></Route>
-        <Route path="*" element={<div>catchall</div>}></Route>
+        <Route
+          path="/login"
+          element={<SignIn updateUserInfo={updateUserInfo} />}
+        ></Route>
+        <Route element={<ProtectedRoute user={user} />}>
+          <Route path="analytics" element={<div>analytics</div>}></Route>
+          <Route path="admin" element={<div>admin</div>}></Route>
+        </Route>
+        <Route
+          path="*"
+          element={
+            <div>
+              <h2>Test</h2>
+              <div>catchall</div>
+            </div>
+          }
+        ></Route>
       </Routes>
-    </section>
+    </>
   );
 }
 
-const Navigation = () => (
-  <nav>
-    <Link to="/">Home</Link> | <Link to="login">Login</Link>
-  </nav>
+const Navigation = ({ user }: NavProps) => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6" style={{ flexGrow: 1 }}>
+        MyApp
+      </Typography>
+      <Button color="inherit" component={Link} to="/">
+        Home
+      </Button>
+      {user ? (
+        <Button color="inherit" component={Link} to="/logout">
+          Logout
+        </Button>
+      ) : (
+        <Button color="inherit" component={Link} to="/login">
+          Login
+        </Button>
+      )}
+      <Button color="inherit" component={Link} to="/admin">
+        Admin
+      </Button>
+    </Toolbar>
+  </AppBar>
 );
 
 export default App;
