7f7ef355db87b93b7b69b90f93962d5af50686bd
asoderlind <axesode@gmail.com>
2023-07-09 21:35:49 +0900

    start working on getting all todos to homepage

--- tssite/src/pages/HomePage.tsx
+++ tssite/src/pages/HomePage.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import AuthContext from "../context/AuthContext";
 
+async function getTodos() {
+  return fetch("http://localhost:8000/api/todos/", {
+    credentials: "include",
+  }).then((res) => res.json());
+}
+
 const HomePage = () => {
-  const { user } = React.useContext(AuthContext);
+  const { user, authTokens } = React.useContext(AuthContext);
+
   return user ? (
     <div>
       <p>You are logged in to the homepage! {user.username}</p>
